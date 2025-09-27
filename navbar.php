4da316b09355e15cd321e2f95b498f386882daf8
avanishrajput12 <avanishrajput208017@gmnail.com>
2025-09-27 21:44:45 +0530

    27-09-2025

--- navbar.php
+++ navbar.php
@@ -72,8 +72,8 @@
     <ul>
       <li><a href="#">About</a></li>
       <li><a href="#">Contact Us</a></li>
-      <li><a href="#">Login</a></li>
-      <li><a href="#">Register</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="register.php">Register</a></li>
     </ul>
   </nav>
 </body>
